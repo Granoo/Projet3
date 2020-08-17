8e1d26703d76d973d677349d141b10ade1de2b8a
Ghaousse <h.ghaousse@laposte.net>
2020-08-17 13:36:22 +0200

    Ajout de l'en-tête et du footer

--- display_details.php
+++ display_details.php
@@ -1,17 +1,39 @@
-<!--NOTES PROVISOIRE-->
-<div id="conteneur_acteur">
-                    <div class="acteur">
-                        <div class="presentation_acteur">
-                                <?php $reponse=$bdd->query('SELECT * FROM acteur');
-                                while ($donnees = $reponse->fetch())
-                                {   ?>
+<?php
+session_start();
 
-                                <figure>
-                                <img class="logo_acteur" src="img/<?php echo $donnees['logo']; ?>" alt="logo_acteur">
-                                </figure>
-                        <p><div class="description"><?php echo $donnees['acteur'];?></div> <br>
-                        <a class="button" href="display_details.php?idacteur=<?php echo $donnees['id_acteur']; ?>">Lire la suite</a>
-                        </p>
-                        </div>
-                    </div>
+$bdd = new PDO('mysql:host=localhost;dbname=gbaf','root','');
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="style.css"/>
+		<link rel="shortcut icon" href="img/gbaf.png" type="image/x-icon">
+		<title>GBAF | Acteurs</title>
+	</head>
+
+	<body>
+        <header class="header_form">
+            <a href="#"><img class="logo" src="img/gbaf.png" alt="logo de GBAF" /></a>    
+            
+            <!--Recupération du nom et prénom de l'utilisateur avec option de mofication ou deconnexion-->
+                <?php
+                if(isset($_SESSION['id']))
+                {
+                ?><br/>
+                <div class="container-user">
+                <a href="user_settings.php"><p>👦 <?php echo $_SESSION['nom'] ?> <?php echo $_SESSION['prenom']?></p></a>
+                <a href="deconnexion.php">↪ Se déconnecter</a>
                 </div>
+                <?php
+                }
+                ?>
+        </header>
+    </body>
+
+    <footer>
+            <p><a href="#">Mentions légales</a> | <a href="#">Contact</a></p>
+    </footer>
+</html>
