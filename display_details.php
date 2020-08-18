b951f9219de35542ed9df30ace16c0def80eaa9e
Ghaousse <h.ghaousse@laposte.net>
2020-08-18 10:45:44 +0200

    Ajout code création partie commentaire non fonctionnel pour le moment

--- display_details.php
+++ display_details.php
@@ -7,14 +7,14 @@ $bdd = new PDO('mysql:host=localhost;dbname=gbaf','root','');
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8"/>
-		<link rel="stylesheet" href="style.css"/>
-		<link rel="shortcut icon" href="img/gbaf.png" type="image/x-icon">
-		<title>GBAF | Acteurs</title>
-	</head>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css"/>
+        <link rel="shortcut icon" href="img/gbaf.png" type="image/x-icon">
+        <title>GBAF | Acteurs</title>
+    </head>
 
-	<body>
+    <body>
         <header class="header_form">
             <a href="#"><img class="logo" src="img/gbaf.png" alt="logo de GBAF" /></a>    
             
@@ -35,7 +35,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=gbaf','root','');
             <!-- Affichage des données de l'acteur -->
 
     <section id="acteur">
-           <h2><?= $donnees['acteur'] ?></h2>
+           <h2></h2>
                <?php $reponse=$bdd->query('SELECT * FROM acteur');
                                 while ($donnees = $reponse->fetch())
                                 {   ?>
@@ -54,6 +54,54 @@ $bdd = new PDO('mysql:host=localhost;dbname=gbaf','root','');
                         </div>
                     </div>
                 </div>
+                
+
+
+            <!-- PHP de la partie commentaire-->
+
+        <?php
+            if(isset($_GET['id']) AND !empty($_GET['id'])) {
+                $getid = htmlspecialchars($_GET['id']);
+                $acteur = $bdd->prepare('SELECT * FROM acteur WHERE id = ?');
+                $acteur->execute(array($getid));
+                $acteur = $acteur->fetch();
+                if(isset($_POST['submit_commentaire'])) {
+                    if(isset($_POST['pseudo'],$_POST['commentaire']) AND !empty($_POST['pseudo']) AND !empty($_POST['commentaire'])) {
+                        $pseudo = htmlspecialchars($_POST['pseudo']);
+                        $commentaire = htmlspecialchars($_POST['commentaire']);
+                        if(strlen($pseudo) < 25) {
+                        $ins = $bdd->prepare('INSERT INTO post (id_user, id_acteur, date_add, post) VALUES (?,?,NOW(),?)');
+                        $ins->execute(array($pseudo,$commentaire,$getid));
+                        $c_msg = "<span style='color:green'>Votre commentaire a bien été posté</span>";
+                    } else {
+                        $c_msg = "Erreur: Le pseudo doit faire moins de 25 caractères";
+                    }
+                } else {
+                    $c_msg = "Erreur: Tous les champs doivent être complétés";
+                }
+            }
+            $commentaires = $bdd->prepare('SELECT * FROM post WHERE id_acteur = ? ORDER BY id DESC');
+            $commentaires->execute(array($getid));
+        ?>
+
+        <!-- Affichage de la partie commentaire-->
+
+            <p><?= $acteur['contenu'] ?></p>
+            <br />
+            <h2>Commentaires:</h2>
+            <form method="POST">
+                <input type="text" name="pseudo" placeholder="Votre pseudo" /><br />
+                <textarea name="commentaire" placeholder="Votre commentaire..."></textarea><br />
+                <input type="submit" value="Poster mon commentaire" name="submit_commentaire" />
+            </form>
+            <?php if(isset($c_msg)) { echo $c_msg; } ?>
+            <br /><br />
+            <?php while($c = $commentaires->fetch()) { ?>
+                <b><?= $c['pseudo'] ?>:</b> <?= $c['commentaire'] ?><br />
+            <?php } ?>
+            <?php
+            }
+            ?>
 
         </section>
     </body>
